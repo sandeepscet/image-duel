8a249d7a818e5eb1e9cf695868be34e5e59118e9
sandip Kachhadiya <sandeep.scet@gmail.com>
2023-05-17 12:14:38 +0530

    minor usability test

--- pages/footer.js
+++ pages/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
@@ -7,10 +8,19 @@ const Footer = () => {
         &copy; {new Date().getFullYear()}{' '}
         <a href="https://chatgpt-prompt-apps.com/" target="_blank">
           Prompt Apps
+          <Image
+            priority
+            src="external.svg"
+            height={16}
+            width={16}
+            alt="external"
+            className="inline"
+          />
         </a>
-        . All rights reserved.
+        {'  '}
+        All rights reserved.
       </p>
-      <p className="mt-2">
+      <p className="mt-2 hidden lg:inline-block">
         Made with{' '}
         <span role="img" aria-label="Love">
           ❤️
